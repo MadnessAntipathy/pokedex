ad3bb4f72ca05202e14732de6fc34974a6e2692f
marcus <marcusteo@hotmail.com>
2019-06-25 00:48:38 +0800

    Added starter pokemon in pokedex

---
+++ main.js
@@ -0,0 +1,13 @@
+
+//function to adjust scroll height and width of frame upon load
+// function load (){
+//   var x = document.getElementById("frameId");
+//   var y = x.contentWindow.document.body.scrollHeight;
+//   var z = x.contentWindow.document.body.scrollWidth;
+//   console.log(y);
+//   console.log(z);
+//   document.getElementById("frameId").style.height = y+"px";
+//   document.getElementById("frameId").style.width = z+"px";
+//   console.log(document.getElementById("frameId").style.height);
+//   console.log(document.getElementById("frameId").style.width);
+// };
